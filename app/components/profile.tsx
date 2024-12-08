a23528d3120c852ffe407e2d57f62f5b633d52b7
Poraparint <poraparint@gmail.com>
2024-12-08 19:10:21 +0700

    new flex-col

--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 function Profile() {
   return (
     <div className="w-full bg-secon flex text-main py-10 pr-5 max-xl:flex-col-reverse gap-8">
-      <div className="flex flex-col gap-9 w-4/6 max-lg:text-base duration-700 max-lg:w-full my-5">
+      <div className="flex flex-col gap-9 w-4/6 max-lg:text-base duration-700 max-xl:w-full my-5">
         {/* Content Section */}
         <div className="flex flex-col gap-3">
           <h1 className="text-4xl font-semibold">PORAPARINT PKAKDEEPHUMMIN</h1>
@@ -57,8 +57,8 @@ function Profile() {
         </div>
       </div>
       {/* Avatar Section */}
-      <div className=" flex flex-col w-2/6 max-lg:w-full justify-center">
-        <div className="relative w-96 h-96 max-lg:w-56 max-lg:h-56 rounded-full flex self-center duration-700">
+      <div className=" flex flex-col w-2/6 max-xl:w-full justify-center">
+        <div className="relative w-96 h-96 max-xl:w-56 max-xl:h-56 rounded-full flex self-center duration-700">
           <Image
             className="rounded-full border-4 border-main"
             src="/Profile.jpeg"
